e219ba8f001b7e6970ee0bc0b713672abf63340e
amendiI <rapahelmuller@hotmail.fr>
2022-07-11 00:07:49 +0200

    ajout de l'algo du score

---
+++ gpo3x/include/score.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+double getscore(graphnode** graph, uint16_t id,uint16_t depth );
---
+++ gpo3x/src/score.cpp
@@ -0,0 +1,25 @@
+#include "score.hpp"
+#include "graphtypes.hpp"
+
+
+int getscore(graphnode** graph, uint16_t id,uint16_t depth){
+
+    
+    //insert provice id dans liste
+
+    //tant que liste non vide 
+
+        //get terrain 
+        //get climate
+        //get trade modif
+        //get cot
+        //get goodmodifier
+        //get unique modifier 
+        //calcule savante de comment on fait quoi pour tout ce brodel
+        //pour tout les voisin de la province
+            ////insert provice id dans liste
+
+    //set de la valeur de la province dans la struct 
+    //return 0
+
+}
